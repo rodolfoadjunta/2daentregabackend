d27fa24076978de7d9858d4fdf92b28e597c4367
rodolfoadjunta <rodolfo.adjunta@gmail.com>
2023-05-31 17:52:00 -0400

    agregando carts.json

--- src/daos/managers/carts.mongo.js
+++ src/daos/managers/carts.mongo.js
@@ -1,109 +1,116 @@
-import { cartsModel } from "../models/cart.model.js";
+import fs from 'fs';
+import path from 'path';
 
-export class CartsMongo{
-    constructor(){
-        this.model = cartsModel;
-    };
-    async create(cart) {
-        try {
-          const result = await this.model.create(cart);
-          return result;
-        } catch (error) {
-          throw new Error(`Error al crear el carito: ${error.message}`);
-        }
-      }
-      
-    async getAll() {
-        try {
-          const carts = await this.model.find().exec();
-          return carts;
-        } catch (error) {
-          throw new Error(`Error al traer todos los caritos: ${error.message}`);
-        }
-      }
+const dataFolderPath = path.resolve('src/data');
+const cartsFilePath = path.join(dataFolderPath, 'carts.json');
 
-      async getById(id) {
-        try {
-          const cart = await this.model.findById(id);
-          return cart;
-        } catch (error) {
-          throw new Error(`Error al traer carros por ID ${error.message}`);
-        }
-      }
-    
-      async update(cartId, updatedData) {
-        try {
-          const cart = await this.getById(cartId);
-          if (!cart) {
-            throw new Error(`No se encuentra el carrito ${cartId}`);
-          }
-          Object.assign(cart, updatedData);
-          const updatedCart = await cart.save();
-          return updatedCart;
-        } catch (error) {
-          throw new Error(`Error al cargar el carrito: ${error.message}`);
-        }
-      }
+export class CartsMongo {
+  async getAll() {
+    try {
+      const cartsData = await fs.promises.readFile(cartsFilePath, 'utf8');
+      const carts = JSON.parse(cartsData);
+      return carts;
+    } catch (error) {
+      throw new Error(`Error al obtener todos los carritos: ${error.message}`);
+    }
+  }
 
-      async delete(cartId) {
-        try {
-          await this.model.findByIdAndDelete(cartId);
-        } catch (error) {
-          throw new Error(`Error al borrar el carrito: ${error.message}`);
-        }
-      }
+  async getById(id) {
+    try {
+      const carts = await this.getAll();
+      const cart = carts.find((cart) => cart.id === id);
+      return cart;
+    } catch (error) {
+      throw new Error(`Error al obtener el carrito por ID: ${error.message}`);
+    }
+  }
 
-      async deleteAllProducts(cartId) {
-        try {
-          await this.model.findByIdAndUpdate(cartId, { products: [] });
-        } catch (error) {
-          throw new Error(`Error al borrar productos del carrito: ${error.message}`);
-        }
-      }
+  async create(cart) {
+    try {
+      const carts = await this.getAll();
+      const newCart = { id: Date.now().toString(), ...cart };
+      carts.push(newCart);
+      await this.saveAll(carts);
+      return newCart;
+    } catch (error) {
+      throw new Error(`Error al crear el carrito: ${error.message}`);
+    }
+  }
 
-      async deleteProduct(cartId, productId) {
-        try {
-          const result = await this.model.findByIdAndUpdate(
-            cartId,
-            { $pull: { products: { productId } } },
-            { new: true }
-          );
-          return result;
-        } catch (error) {
-          throw new Error(`Error borrando productos del carrito: ${error.message}`);
-        }
+  async update(cartId, updatedData) {
+    try {
+      const carts = await this.getAll();
+      const cartIndex = carts.findIndex((cart) => cart.id === cartId);
+      if (cartIndex === -1) {
+        throw new Error(`No se encuentra el carrito con ID ${cartId}`);
       }
+      const updatedCart = { ...carts[cartIndex], ...updatedData };
+      carts[cartIndex] = updatedCart;
+      await this.saveAll(carts);
+      return updatedCart;
+    } catch (error) {
+      throw new Error(`Error al actualizar el carrito: ${error.message}`);
+    }
+  }
 
-      
-      async update(cartId, products) {
-        try {
-          const updatedCart = await this.model.findByIdAndUpdate(
-            cartId,
-            { products },
-            { new: true }
-          );
-          return updatedCart;
-        } catch (error) {
-          throw new Error(`Error cargando el carrito: ${error.message}`);
-        }
+  async delete(cartId) {
+    try {
+      const carts = await this.getAll();
+      const cartIndex = carts.findIndex((cart) => cart.id === cartId);
+      if (cartIndex === -1) {
+        throw new Error(`No se encuentra el carrito con ID ${cartId}`);
       }
+      carts.splice(cartIndex, 1);
+      await this.saveAll(carts);
+    } catch (error) {
+      throw new Error(`Error al eliminar el carrito: ${error.message}`);
+    }
+  }
 
-      async updateProductQuantity(cartId, productId, quantity) {
-        try {
-          const updatedCart = await this.model.findOneAndUpdate(
-            { _id: cartId, "products.productId": productId },
-            { $set: { "products.$.quantity": quantity } },
-            { new: true }
-          );
-      
-          if (!updatedCart) {
-            throw new Error("Carrito o producto no se encuentra");
-          }
-      
-          return updatedCart;
-        } catch (error) {
-          throw new Error(`Error cargando cantidad de producto: ${error.message}`);
-        }
+  async deleteProduct(cartId, productId) {
+    try {
+      const carts = await this.getAll();
+      const cart = carts.find((cart) => cart.id === cartId);
+      if (!cart) {
+        throw new Error(`No se encuentra el carrito con ID ${cartId}`);
       }
-      
+      const productIndex = cart.products.findIndex((product) => product.productId === productId);
+      if (productIndex === -1) {
+        throw new Error(`No se encuentra el producto con ID ${productId} en el carrito`);
+      }
+      cart.products.splice(productIndex, 1);
+      await this.saveAll(carts);
+    } catch (error) {
+      throw new Error(`Error al eliminar el producto del carrito: ${error.message}`);
+    }
+  }
+
+  async saveAll(carts) {
+    try {
+      const cartsData = JSON.stringify(carts, null, 2);
+      await fs.promises.writeFile(cartsFilePath, cartsData);
+    } catch (error) {
+      throw new Error(`Error al guardar los carritos: ${error.message}`);
+    }
+  }
+
+  async updateProductQuantity(cartId, productId, quantity) {
+    try {
+      const carts = await this.getAll();
+      const cart = carts.find((cart) => cart.id === cartId);
+      if (!cart) {
+        throw new Error(`No se encuentra el carrito con ID ${cartId}`);
+      }
+      const product = cart.products.find((product) => product.productId === productId);
+      if (!product) {
+        throw new Error(`No se encuentra el producto con ID ${productId} en el carrito`);
+      }
+      product.quantity += quantity; // Sumar la cantidad al producto existente
+      await this.saveAll(carts);
+      return cart;
+    } catch (error) {
+      throw new Error(`Error al actualizar la cantidad del producto en el carrito: ${error.message}`);
+    }
+  }
+  
 }
